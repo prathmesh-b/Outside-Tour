411b77fb376b5b13b286823990e2d593d91b2e53
prathmesh1999 <59602024+prathmesh1999@users.noreply.github.com>
2020-09-21 23:14:21 +0530

    First version of Website

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+// open the menu on click 
+selectElement(s: '.open').addEventListener('click', () => {
+selectElement(s: '.nav-list).classList.add('active');
+});
+
+/*Close the menu on click */
+selectElement(s: '.close').addEventListener('click', () => {
+selectElement(s: '.nav-list).classList.remove('active');
+});
